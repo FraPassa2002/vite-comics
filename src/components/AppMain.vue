5c5a6091e6aa6ad73e39d7c90bf3ce77799cf522
Passarelli / Francesco Passarelli <passarellif72@gmail.com>
2024-01-19 18:56:19 +0100

    main completato

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -26,7 +26,82 @@
                         name: 'DC POWER VISA',
                         image: '../../public/img/buy-dc-power-visa.svg'
                     },
-                ]
+                ],
+
+                comics: [
+  {
+      "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+      "price": "$19.99",
+      "series": "Action Comics",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+      "price": "$3.99",
+      "series": "American Vampire 1976",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+      "price": "$16.99",
+      "series": "Aquaman",
+      "type": "graphic novel",
+  },
+  {
+      "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+      "price": "$2.99",
+      "series": "Batgirl",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+      "price": "$3.99",
+      "series": "Batman",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+      "price": "$2.99",
+      "series": "Batman Beyond",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+      "price": "$3.99",
+      "series": "Batman/Superman",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+      "price": "$4.99",
+      "series": "Batman/Superman Annual",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+      "price": "$5.99",
+      "series": "Batman: The Joker War Zone",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+      "price": "$6.99",
+      "series": "Batman: Three Jokers",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+      "price": "$4.99",
+      "series": "Batman: White Knight Presents: Harley Quinn",
+      "type": "comic book",
+  },
+  {
+      "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+      "price": "$16.99",
+      "series": "Catwoman",
+      "type": "graphic novel",
+  },
+]
             };
         },
         //  components: {
@@ -38,12 +113,29 @@
 <template>
     <main>
         <section id="Content-container">
-            <div class="container">
-                <h1>
-                    <a href="#">
-                    Content goes here 
-                    </a>
-                </h1>
+            <div id="jumbotron">        
+            </div>
+            <div class="series-container">
+                <div class="container">
+                    <div class="h1-container">
+                        <h1>
+                            CURRENT SERIES
+                        </h1>
+                    </div>
+
+                    <div class="products-container" >
+                        <div class="single-product" v-for="(comic, i) in comics">
+                            <div>
+                                <img :src="comic.thumb" :alt="comic.series">
+                            </div>
+                            <div>
+                                <p>
+                                    {{ comic.series }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
         <section id="market-container">
@@ -122,5 +214,36 @@
 </template>
 
 <style lang="scss" scoped>
-    
+.single-product {
+    padding: 30px;
+    width: calc(100% / 5);
+    transition-duration: 0.3s;
+
+    &:hover {
+        transform: scale(1.1);
+        cursor: pointer;
+    }
+
+    > div:first-child {
+        width: 200px;
+        height: 200px;
+        margin-bottom: 15px;
+        overflow: hidden;
+
+        img {
+            height: 100%;
+            object-fit:fill;
+        }
+    }
+
+    >div:last-child > * {
+        text-transform: uppercase;
+    }
+}
+
+.products-container {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 50px 0;
+}
 </style>
